44ee97753a7f013a1e9f861482cc45a5a351d7d9
DavinderPRO <38653626+DavinderPRO@users.noreply.github.com>
2018-04-25 16:25:51 +0530

    Create tinder_autoswipe.js

---
+++ tinder_autoswipe.js
@@ -0,0 +1,5 @@
+timer = setInterval(swiperight,3000);
+		  function swiperight(){
+          var elem = document.getElementsByClassName("recsGamepad__button--like");
+          elem[0].click();
+          }
